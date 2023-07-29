0fb13b30801c74f09e31f37c5869f4e75eb7b5af
Nakatai-0322 <nakatai0322@gmail.com>
2023-07-29 09:16:29 +0900

    Update index.py

--- index.py
+++ index.py
@@ -6,15 +6,18 @@
 import tweepy
 import Config
 
+
 def main():
     auth = tweepy.OAuthHandler(Config.consumer_key, Config.consumer_secret)
     auth.set_access_token(Config.access_token, Config.access_token_secret)
     api = tweepy.API(auth)
     for User in Config.toUsers:
         recipient_id = User
-        result = api.send_direct_message(recipient_id=recipient_id, text=Config.message)
+        result = api.send_direct_message(
+            recipient_id=recipient_id, text=Config.message)
         print(result.id)
         print(Config.message)
+
 
 if __name__ == "__main__":
     main()
